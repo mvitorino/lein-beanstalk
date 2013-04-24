ee4ea398dc2ae79a07e88a828cf9ac5968a119bd
Miguel Vitorino <miguel.vitorino@bluebridgeltd.com>
2013-04-24 17:26:41 +0100

    adding S3 snapshot repository from bluebridge

--- project.clj
+++ project.clj
@@ -4,4 +4,9 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [com.amazonaws/aws-java-sdk "1.4.2"]
                  [lein-ring "0.8.3"]]
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :plugins [[s3-wagon-private "1.1.2"]]
+  :repositories [["private" {:url "s3p://com.blue.mvn/releases"
+                             :username :env/AWS_ACCESS_KEY
+                             :passphrase :env/AWS_SECRET_KEY}]]
+)
